7764feb8c0b9e54f9c57b7040df5600294fc37ae
RahatMinhaz <minhaz7591@gmail.com>
2021-10-31 03:55:54 +0600

    something changed

--- index.js
+++ index.js
@@ -30,6 +30,23 @@ async function run(){
 
     }
 }
+async function run(){
+    try{
+        await client.connect();
+        const database = client.db('food_list');
+        const foodCollection = database.collection('foodlist');
+
+
+        app.get('/foodlist', async(req,res) =>{
+            const cursor = foodCollection.find({});
+            const foodItems = await cursor.toArray();
+            res.send(foodItems);
+        })
+    }
+    finally{
+
+    }
+}
 run().catch(console.dir);
 
 app.get('/', (req,res) =>{
