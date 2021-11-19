cd82697ac5cafd0aa8ed45875cb1068564553529
RahatMinhaz <minhaz7591@gmail.com>
2021-11-19 18:03:06 +0600

    done again

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ async function run(){
         app.delete('/usersinfo2/:id', async(req,res) =>{
             const id = req.params.id;
             const query = {_id: ObjectId(id)};
-            const result = await userInfo.deleteOne(query);
+            const result = await userInfo2.deleteOne(query);
             res.json(result);
         })
 
